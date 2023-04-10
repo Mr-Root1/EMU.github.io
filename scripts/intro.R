ae46adaffe4bdda3ab8d348e5b986dc3209cc51b
Mr-Root1 <romanescobar1029@gmail.com>
2023-04-10 12:02:28 -0500

    n3

--- scripts/intro.R
+++ scripts/intro.R
@@ -1,5 +1,6 @@
 # from CRAN
 install.packages("blogdown")
-
+install.packages("bslib")
+install.packages("bslib", dependencies = TRUE)
 # or the development version from GitHub
 devtools::install_github("rstudio/blogdown")
